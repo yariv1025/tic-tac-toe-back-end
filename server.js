a0f1fb0f61abbe634233bf4743f1835152d32067
Yariv Garala <yariv1052@gmail.com>
2021-06-17 20:12:22 +0300

    Edit configuration

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 // create express app
 const app = express();
-
+const port = 3001;
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -20,7 +20,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(dbConfig.url, {
 	useNewUrlParser: true
 }).then(() => {
-    console.log("Successfully connected to the database");    
+    console.log("Successfully connected to the database");
 }).catch(err => {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
@@ -28,12 +28,12 @@ mongoose.connect(dbConfig.url, {
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    res.json({"message": "Welcome to Tic-Tac-Toe Data Base application."});
 });
 
 require('./app/routes/note.routes.js')(app);
 
 // listen for requests
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
 });
